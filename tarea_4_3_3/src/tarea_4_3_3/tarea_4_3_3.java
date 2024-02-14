4e6dec326682331035a5b7eca66a309e7e98a186
mxrtxx <marta21700@gmail.com>
2024-02-14 09:55:59 +0100

    Versión cero del proyecto

---
+++ tarea_4_3_3/src/tarea_4_3_3/tarea_4_3_3.java
@@ -0,0 +1,21 @@
+package tarea_4_3_3;
+
+import java.util.Scanner;
+
+public class tarea_4_3_3 {
+		static public void main (String[]args){
+	        Scanner Teclado=new Scanner(System.in);
+	        int numero1=0, contador=0;
+	        System.out.println("Dame un numero para saber su factorial");
+	        numero1=Teclado.nextInt();
+	        float resultado=0;
+	        float multiplicador=1;
+	        while (numero1>1){
+	            resultado=multiplicador*numero1;
+	            multiplicador=resultado;
+	            numero1--;
+	            System.out.println(resultado);
+	        }
+	    }
+	}
+
